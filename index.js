56681593a2ce7c7aafb886603f07187e652df03b
Vinod1999 <109039225+vinodyeldur@users.noreply.github.com>
2023-03-15 09:58:01 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+
+
+function lifeInWeeks(age) {
+  var remainingDays = 90 - age;
+  var days = remainingDays * 365;
+  var weeks = remainingDays * 52;
+  var months = remainingDays * 12;
+
+  console.log("You have "+ days + " days, "+ weeks + " weeks and "+ months + " months left ")
+    
+}
+lifeInWeeks(51);
+    
+    
+    
+    
+    
+
